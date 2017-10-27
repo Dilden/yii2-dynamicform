d9d4bd175cd3dc41b690d8bd46b0beb479dca851
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2017-10-27 11:40:11 -0500

    trying both fixes

--- src/assets/yii2-dynamic-form.js
+++ src/assets/yii2-dynamic-form.js
@@ -34,7 +34,7 @@
     var methods = {
         init: function (widgetOptions) {
             return this.each(function () {
-                widgetOptions.template = _parseTemplate(widgetOptions);
+                widgetOptions.template = _parseTemplate(widgetOptions, false);
             });
         },
 
@@ -43,6 +43,7 @@
         },
 
         cloneItem: function (widgetOptions, e, $elem) {
+            widgetOptions.template = _parseTemplate(widgetOptions, true);
            _addItem(widgetOptions, e, $elem, true);
         },
 
@@ -58,7 +59,7 @@
         }
     };
 
-    var _parseTemplate = function(widgetOptions) {
+    var _parseTemplate = function(widgetOptions, duplicate) {
 
         var $template = $(widgetOptions.template);
         $template.find('div[data-dynamicform]').each(function(){
@@ -69,26 +70,27 @@
             }
         });
 
-        
-        $template.find('input, textarea, select').each(function() {
-            if ($(this).is(':checkbox') || $(this).is(':radio')) {
-                var type         = ($(this).is(':checkbox')) ? 'checkbox' : 'radio';
-                var inputName    = $(this).attr('name');
-                var $inputHidden = $template.find('input[type="hidden"][name="' + inputName + '"]').first();
-                var count        = $template.find('input[type="' + type +'"][name="' + inputName + '"]').length;
+        if(!duplicate) {
+            $template.find('input, textarea, select').each(function() {
+                if ($(this).is(':checkbox') || $(this).is(':radio')) {
+                    var type         = ($(this).is(':checkbox')) ? 'checkbox' : 'radio';
+                    var inputName    = $(this).attr('name');
+                    var $inputHidden = $template.find('input[type="hidden"][name="' + inputName + '"]').first();
+                    var count        = $template.find('input[type="' + type +'"][name="' + inputName + '"]').length;
 
-                if ($inputHidden && count === 1) {
-                    $(this).val(1);
-                    $inputHidden.val(0);
+                    if ($inputHidden && count === 1) {
+                        $(this).val(1);
+                        $inputHidden.val(0);
+                    }
+
+                    $(this).prop('checked', false);
+                } else if($(this).is('select')) {
+                    $(this).find('option:selected').removeAttr("selected");
+                } else {
+                    $(this).val(''); 
                 }
-
-                $(this).prop('checked', false);
-            } else if($(this).is('select')) {
-                $(this).find('option:selected').removeAttr("selected");
-            } else {
-                $(this).val(''); 
-            }
-        });
+            });
+        }
 
         // remove "error/success" css class
         var yiiActiveFormData = $('#' + widgetOptions.formId).yiiActiveForm('data');
